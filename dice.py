dbbf5881d038fc4e0c5fdf8385da28b164ceafee
Nathan Solis <113551835+nasolis665@users.noreply.github.com>
2023-03-29 16:28:20 +0000

    Perfect

--- dice.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-def roll(num, dice):
-  if num in dice:
-    num=num.replace('d','')
-    num = randint(1,int(num))
-    print("You rolled a: " + str(num))
-  else:
-    print("That is not a valid number, pick a valid number")
--- story.py
+++
@@ -1,2 +0,0 @@
-def story_line():
-  input("Hello adventurer, welcome to (made up place). I am Godfrey and I will be you're guide through this adventure")
